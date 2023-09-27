580133da15aacad898e855a61d9f9dab8d07dafc
NeK0toKage <14210122@g.matsuyama-u.ac.jp>
2023-09-28 07:07:51 +0900

    third commit

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -10,13 +10,19 @@ class TodoController extends Controller
 {
     public function todolist(Todo $todo,Sub_todo $sub_todo, Request $request)
     {
-        return view('record.todolist')->with(['todoss' => $todo->get(),'sub_todos' => $sub_todo->get()]);
+        return view('record.todolist')->with(['todoss' => $todo->get(), 'detailsub'=> $todo->get(), 'sub_todos' => $sub_todo->get()]);
     }
     
     //public function todolis(Todo $todo)
     //{
         //return view('record.todolist')->with(['todos' =>$todo->getByLimit()]);
     //}
+    
+    public function detailsub(Todo $todo,Sub_todo $sub_todo)
+    {
+        $todo->test='17';
+        return view('record.detailandsub')->with(['mainTodo'=> $todo,'subTodos'=>$sub_todo->where('todo_id','=',$todo->id)->get()]);
+    }
 
 }
 
